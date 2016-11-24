8359e36907be172462410593f4757dd9ad79519d
barryz <barryzxb@gmail.com>
2016-11-24 19:42:46 +0800

    add PostJSONWithParams

--- httplib/post.go
+++ httplib/post.go
@@ -8,6 +8,62 @@ import (
 	"net/http"
 )
 
+// PostJSONRequest new http request for post json
+type PostJSONRequest struct {
+	req *http.Request
+}
+
+// SetHeader set new header to PostJSONRequest
+func (req *PostJSONRequest) SetHeader(k, v string) {
+	req.req.Header.Set(k, v)
+}
+
+// SetBasicAuth set a basic authentication to PostJSONRequest
+func (req *PostJSONRequest) SetBasicAuth(u, p string) {
+	req.req.SetBasicAuth(u, p)
+}
+
+// DoRequest send a request
+func (req *PostJSONRequest) DoRequest() (response []byte, err error) {
+	var resp *http.Response
+	client := &http.Client{}
+	resp, err = client.Do(req.req)
+	if err != nil {
+		return
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode >= 300 {
+		err = errors.New("get invalid response status code")
+		return
+	}
+
+	response, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return
+	}
+	return
+}
+
+// PostJSONWithParams PostJson with some headers or params
+func PostJSONWithParams(url string, v interface{}) (newreq *PostJSONRequest, err error) {
+	bs, err := json.Marshal(v)
+	if err != nil {
+		return
+	}
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(bs))
+	if err != nil {
+		return
+	}
+
+	newreq = &PostJSONRequest{req: req}
+
+	return
+}
+
+// PostJSON Post Json object
 func PostJSON(url string, v interface{}) (response []byte, err error) {
 	var bs []byte
 	bs, err = json.Marshal(v)
